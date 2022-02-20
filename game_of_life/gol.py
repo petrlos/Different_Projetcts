e4eb3a4d438d4d421147bc1fabafa2d94d3a00f5
Petr Los <petrlos@gmail.com>
2022-02-20 16:05:06 +0100

    GoL: GoL working

--- game_of_life/gol.py
+++ game_of_life/gol.py
@@ -1,23 +1,17 @@
 #Game of Life
 from gamefield import GameField
-from copy import deepcopy
 
-def getNeighbourCount(row, column):
-    counter = 0
-    directions = [(-1,-1), (-1,0), (-1,1), (1,-1), (1,0), (1,1), (0,-1), (0,1)]
-    for direction in directions:
-        deltaX, deltaY = direction
-        neighbourCoords = (row + deltaX, column + deltaY)
-        if neighbourCoords in startField.field.keys():
-            counter += startField.field[neighbourCoords]
-    return counter
+def GoLstep(startField, size):
+    def getNeighbourCount(row, column):
+        counter = 0
+        directions = [(-1, -1), (-1, 0), (-1, 1), (1, -1), (1, 0), (1, 1), (0, -1), (0, 1)]
+        for direction in directions:
+            deltaX, deltaY = direction
+            neighbourCoords = (row + deltaX, column + deltaY)
+            if neighbourCoords in startField.field.keys():
+                counter += startField.field[neighbourCoords]
+        return counter
 
-size = 6
-
-startField = GameField(size, True)
-print(startField)
-
-for _ in range(5):
     newField = GameField(size)
     for row in range(startField.size):
         for column in range(startField.size):
@@ -26,5 +20,4 @@ for _ in range(5):
                 newField.field[(row, column)] = 1
             elif not startField.field[(row, column)] and neighbours == 3:
                 newField.field[(row, column)] = 1
-    startField = deepcopy(newField)
-    print(newField)
+    return newField
