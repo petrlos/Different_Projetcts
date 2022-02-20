15374c98bbfc16fd4f654063bcbaec3c9f4d3363
Petr Los <petrlos@gmail.com>
2022-02-20 16:51:44 +0100

    GoL: Pyglet error

--- game_of_life/main.py
+++ game_of_life/main.py
@@ -1,13 +1,27 @@
 #Game of Life
-
 from gamefield import GameField
 from gol import GoLstep
+import pyglet
 
 size = 6
 
-startField = GameField(size, True)
-print(startField)
+window = pyglet.window.Window(600,400, caption="Game of Life")
+label = pyglet.text.Label()
 
-for _ in range(5):
-    startField = GoLstep(startField, size)
-    print(startField)
+field = GameField(size, True)
+label.text = "blah"
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+@window.event #enter closes window
+def on_key_press(key_code, modifier):
+    if key_code == pyglet.window.key.ENTER:
+        window.close()
+    if key_code == pyglet.window.key.SPACE:
+        field = GoLstep(field, size)
+
+
+pyglet.app.run()
