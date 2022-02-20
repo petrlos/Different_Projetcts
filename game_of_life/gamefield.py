17e8d7c794de016daa5062b8df70c7242815efb5
Petr Los <petrlos@gmail.com>
2022-02-20 15:56:03 +0100

    GoL: GoL working

--- game_of_life/gamefield.py
+++ game_of_life/gamefield.py
@@ -1,25 +1,29 @@
 class GameField:
-    def __init__(self):
-        self.size = 0
+    def __init__(self, size, readFromFile=False):
+        self.size = size
         self.field = {}
+        for row in range(size):
+            for column in range(size):
+                self.field[(row, column)] = 0
+        if readFromFile:
+            self.__readFromFile()
 
     def __str__(self):
         description = ""
         for row in range(self.size):
             for column in range(self.size):
                 if (row, column) in self.field.keys():
-                    description += self.field[(row, column)]
-                else:
-                    description += "."
+                    description += "#" if self.field[(row, column)] else "."
                 description += " "
             description += "\n"
         return description
 
-    def readFromFile(self):
+    def __readFromFile(self):
         with open("text.txt") as file:
             lines = file.read().splitlines()
         for row, line in enumerate(lines):
             for column, char in enumerate(line):
                 if char == "1":
-                    self.field[(row, column)] = "#"
-        self.size = row + 1
+                    self.field[(row, column)] = 1
+                else:
+                    self.field[(row, column)] = 0
--- game_of_life/gol.py
+++ game_of_life/gol.py
@@ -1,7 +1,30 @@
 #Game of Life
 from gamefield import GameField
+from copy import deepcopy
 
+def getNeighbourCount(row, column):
+    counter = 0
+    directions = [(-1,-1), (-1,0), (-1,1), (1,-1), (1,0), (1,1), (0,-1), (0,1)]
+    for direction in directions:
+        deltaX, deltaY = direction
+        neighbourCoords = (row + deltaX, column + deltaY)
+        if neighbourCoords in startField.field.keys():
+            counter += startField.field[neighbourCoords]
+    return counter
 
-field = GameField()
-field.readFromFile()
-print(field)
+size = 6
+
+startField = GameField(size, True)
+print(startField)
+
+for _ in range(5):
+    newField = GameField(size)
+    for row in range(startField.size):
+        for column in range(startField.size):
+            neighbours = getNeighbourCount(row, column)
+            if startField.field[(row, column)] and neighbours in [2,3]:
+                newField.field[(row, column)] = 1
+            elif not startField.field[(row, column)] and neighbours == 3:
+                newField.field[(row, column)] = 1
+    startField = deepcopy(newField)
+    print(newField)
