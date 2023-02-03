b0ab9d80db9ae3fb8e06d7b1a1643ad1a4da34b8
Petr Los <petrlos@gmail.com>
2023-02-03 23:43:25 +0100

    Recursion

--- recursion.py
+++ recursion.py
@@ -26,3 +26,26 @@ def x_to_power_of_y(x,y):
         return x * x_to_power_of_y(x,y-1)
     else:
         return 1
+
+def check_prime(start):
+    def is_prime(start, number):
+        if number == 1:
+            return True
+        elif start % number == 0 and start != number:
+            return False
+        else:
+            return is_prime(start, number-1)
+
+    return is_prime(start, start-1)
+
+def gcd_recursion(number1, number2):
+    def gcd_check(number1, number2, gcd):
+        if number1 % gcd == 0 and number2 % gcd == 0:
+            return gcd
+        else:
+            return gcd_check(number1, number2, gcd-1)
+    if number1 > number2:
+        number1, number2 = number2, number1
+    return gcd_check(number1, number2, number2-1)
+
+print(gcd_recursion(24,54))
